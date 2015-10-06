3937d5a21b4f45dac9bba1bde9916f58807a47b3
k4hun <thameth@gmail.com>
2015-10-06 07:11:41 +0000

    test fix

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -34,12 +34,6 @@ class LineItemsControllerTest < ActionController::TestCase
       assert_select 'tr#current_item td', /Programming Ruby 1.9/
     end
   end
-  
-  test "markup needed for store.js.coffee is in place" do
-    get :index
-    assert_select '.store .entry img', 2
-    assert_select '.entry input[type=submit]', 2
-  end
 
   test "should show line_item" do
     get :show, id: @line_item
